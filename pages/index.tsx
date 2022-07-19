864246de0032596bdf39107c4dc34667de27b724
joaobr200 <joaoarantesjob@gmail.com>
2022-07-19 11:30:11 -0300

    Home páge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
 import type { NextPage } from "next";
 import { styled } from "../stitches.config";
+import Layout from "../components/Ui/Layout";
+import Seo from "../components/Seo";
+const Box = styled("div", {
+  gridColumn: "2/3",
+  gridRow: "2/5",
+  backgroundColor: "red ",
+});
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>olá</h1>
-    </div>
+    <Layout>
+      <Seo title="Cardápio Digital" description="Cardápio Digital" />
+      <Box>
+        <div style={{ height: "200px" }}>
+          <h1>testando</h1>
+        </div>
+        <div style={{ height: "200px" }}>
+          <h1>testando</h1>
+        </div>
+        <div style={{ height: "200px" }}>
+          <h1>testando</h1>
+        </div>
+        <div style={{ height: "200px" }}>
+          <h1>testando</h1>
+        </div>
+        <div style={{ height: "200px" }}>
+          <h1>testando</h1>
+        </div>
+      </Box>
+    </Layout>
   );
 };
 
