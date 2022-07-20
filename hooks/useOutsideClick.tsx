dc19057ab90df283cd8564754b27f0738622c0a3
joaobr200 <joaoarantesjob@gmail.com>
2022-07-20 19:56:16 -0300

    remove logss

--- hooks/useOutsideClick.tsx
+++ hooks/useOutsideClick.tsx
@@ -12,16 +12,11 @@ function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   useEventListener(mouseEvent, (event) => {
     const el = ref?.current;
 
-    console.log(event.target);
-    console.log(el);
-
     // Do nothing if clicking ref's element or descendent elements
     if (el?.contains(event.target as Node)) {
-      console.log("devo fechar");
       handler(event);
       return;
     }
-    console.log("nao vou fechar");
 
     return;
   });
